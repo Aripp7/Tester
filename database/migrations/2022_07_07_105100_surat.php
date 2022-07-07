58ffb9db0cb83170df4e975cd310eade6674e067
Ahmad Arif <ahmadarif9085@gmail.com>
2022-07-07 18:22:40 +0700

    add source 'Surat'

---
+++ database/migrations/2022_07_07_105100_surat.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Surat extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surats', function (Blueprint $table) {
+            $table->integer('id_surat');
+            $table->string('jenis_surat', 50);
+            $table->string('nomor_surat', 50);
+            $table->date('tgl_surat');
+            $table->string('tujuan', 50);
+            $table->string('keterangan', 50);
+            $table->string('file_surat', 50);
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+}
